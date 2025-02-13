6199bc9521a9390ee8386ae75829fdab15e219f6
Deiker <deiker@gmail.com>
2025-02-13 11:50:50 -0400

    Modificacion de APP_MARCA

--- app/static/Js/marcas.js
+++ app/static/Js/marcas.js
@@ -183,7 +183,7 @@ function toggleLists() {
             $.ajax({
             
                     type: 'POST',
-                    url: '/marcas/apigeneral',
+                    url: '/marcas/marcasdetalles',
                     contentType: 'application/json',
                     data: JSON.stringify({FechaInicio:fecha_inicio,
                                           FechaFin:fecha_fin,
@@ -194,29 +194,60 @@ function toggleLists() {
                           
                             console.log(response)
                             // Mostrar en consola la descripción de cada item recibido
-                            const usd_marca =response.MarcaUSD[0].Total_Ventas_USD
-                            const bs_marca = response.MarcaBs[0].Total_Ventas
-                            const trasaciones = response.MarcaUSD[0].Total_Transacciones
+                            const usd_marca =response.Total_USD
+                            const trasaciones = response.cantidades
                             
-                            const formatted_bs = new Intl.NumberFormat('en-US', { style: 'decimal', maximumFractionDigits: 2 }).format(bs_marca);
                             const formatted_usd = new Intl.NumberFormat('en-US', { style: 'decimal', maximumFractionDigits: 2 }).format(usd_marca);
+                            const formatted_transa = new Intl.NumberFormat('en-US', { style: 'decimal', maximumFractionDigits: 2 }).format(trasaciones);
                             
                             document.getElementById('totalSalesUSD').innerText = `$ ${formatted_usd}`
-                            document.getElementById('totalSalesBS').innerText = `Bs ${formatted_bs}`
-                            document.getElementById('transacciones').innerText = trasaciones
-                            hideLoadingModal();
+                            document.getElementById('transacciones').innerText = formatted_transa
+                            
                         },
                         error: function(error) {
                             console.error('Error:', error);
                             hideLoadingModal();
                         }
                     });
+                    $.ajax({
+            
+                        type: 'POST',
+                        url: '/marcas/marcasdetallesbs',
+                        contentType: 'application/json',
+                        data: JSON.stringify({FechaInicio:fecha_inicio,
+                                              FechaFin:fecha_fin,
+                                              marca:marca
+                            }),
+                            success: function(response) {
+                
+                              
+                                console.log(response)
+                                // Mostrar en consola la descripción de cada item recibido
+                                const bs_marca =response.Total_BS
+                                
+                                const formatted_bs = new Intl.NumberFormat('en-US', { style: 'decimal', maximumFractionDigits: 2 }).format(bs_marca);
+                                
+                                document.getElementById('totalSalesBS').innerText = `$ ${formatted_bs}`
+                                hideLoadingModal();
+
+                            },
+                            error: function(error) {
+                                console.error('Error:', error);
+                                hideLoadingModal();
+                            }
+                        });
+
+
             })
+            
+            
             
             
 
             const btn_detalles = document.getElementById('btn-detalles').addEventListener('click', () =>{
-            
+                if (storesChart) {
+                    storesChart.destroy();
+                }
                 const fecha_inicio = document.getElementById('startDate').value
                 const fecha_fin = document.getElementById('endDate').value
                 const marca = document.getElementById('marca').value
@@ -224,7 +255,7 @@ function toggleLists() {
                 $.ajax({
                     
                         type: 'POST',
-                        url: '/marcas/tiendas',
+                        url: '/marcas/marcasdetallesxtiendas',
                         contentType: 'application/json',
                         data: JSON.stringify({FechaInicio:fecha_inicio,
                                             FechaFin:fecha_fin,
@@ -234,83 +265,118 @@ function toggleLists() {
                                 
                                 let ventasUsd  = []
                                 let label = []
-                                response.forEach(item => {
+                            const tiendas = Object.keys(response)
+                            console.log(tiendas);
+
+                            const cantidades = [];
+                            const totalesUSD = [];
                             
-                                console.log(`${item.Total_Ventas_USD}`, `${item.tienda}`);
-                                ventasUsd.push(item.Total_Ventas_USD)
-                                label.push(item.tienda)
-                                
-                            });
-                            const combined = ventasUsd.map((venta, index) => ({
-                                venta,
-                                label: label[index]
-                            }));
-                            combined.sort((a, b) => b.venta - a.venta);
-                            ventasUsd = combined.map(item => item.venta);
-                            label = combined.map(item => item.label);
-            
-                                
-                            hideLoadingModal();
-                                const storesCanvas = document.getElementById("storesChart");
-                                const storesCtx = storesCanvas.getContext("2d");
-            
-                                if (storesChart) {
-                                storesChart.destroy();
+                            // Iterar sobre las tiendas para llenar los arrays
+                            for (const tienda in response) {
+                              if (response.hasOwnProperty(tienda)) {
+                                cantidades.push(response[tienda].cantidad);
+                                totalesUSD.push(response[tienda].total_USD);
+                              }
                             }
-            
-                                // Crear el gráfico
-                                storesChart = new Chart(storesCtx, {
+                            console.log("Cantidades:", cantidades);
+                            console.log("Totales en USD:", totalesUSD);
+                                
+
+                            hideLoadingModal();
+
+                            const storesCanvas = document.getElementById("storesChart");
+                            const storesCtx = storesCanvas.getContext("2d");
+
+                            const combinedData = Object.keys(response).map((tienda) => ({
+                                label: tienda,
+                                sales: response[tienda].total_USD,
+                                transactions: response[tienda].cantidad
+                              }));
+                              
+
+                            const sortedCombinedData = combinedData.sort((a, b) => b.sales - a.sales);
+  
+                            // Extraer los datos ordenados
+                            const sortedLabels = sortedCombinedData.map(item => item.label);
+                            const sortedSales = sortedCombinedData.map(item => item.sales);
+                            const sortedTrans = sortedCombinedData.map(item => item.transactions);
+
+                            storesChart = new Chart(storesCtx, {
                                 type: "bar",
                                 data: {
-                                    labels: label,
-                                    datasets: [
-                                
-                                
+                                  labels: sortedLabels, // Etiquetas ordenadas por ventas
+                                  datasets: [
                                     {
-                                        label: "Ventas Totales $",
-                                        borderColor: "#006400",
-                                        backgroundColor: "#1E3A8A",
-                                        data: ventasUsd,
-                                        borderRadius: 4,
-                                        fill: true,
+                                      label: "Unidades Vendidas",
+                                      backgroundColor: "rgba(99, 102, 241, 0.6)", // Color de fondo de las barras
+                                      data: sortedTrans, // Datos de transacciones en el orden correcto
+                                      borderRadius: 4,
+                                      fill: true, // Llenar el área debajo de las barras
                                     },
-                                    ],
+                                    {
+                                      label: "Ventas Totales ($)",
+                                      borderColor: "#006400",
+                                      backgroundColor: "#1E3A8A",
+                                      data: sortedSales, // Datos de ventas totales en el orden correcto
+                                      borderRadius: 4,
+                                      fill: true, // Llenar el área debajo de las barras
+                                    },
+                                  ],
                                 },
                                 options: {
-                                    responsive: true,
-                                    maintainAspectRatio: false,
-                                    scales: {
-                                        x: {
-                                        stacked: true,
-                                        ticks: {
-                                            maxRotation: 45,
-                                            minRotation: 20,
-                                            font: {
-                                            size: function () {
-                                                // Obtener el ancho de la pantalla
-                                                const chartWidth = window.innerWidth; 
-                                                return chartWidth > 450 ? 12 : 7; // Ajustar a 10px si es mayor a 360px, sino 7px
-                                            },
-                                            },
+                                  responsive: true,
+                                  maintainAspectRatio: false,
+                                  scales: {
+                                    x: {
+                                      stacked: true,
+                                      ticks: {
+                                        maxRotation: 45,
+                                        minRotation: 20,
+                                        font: {
+                                          size: function () {
+                                            // Obtener el ancho de la pantalla
+                                            const chartWidth = window.innerWidth; 
+                                            return chartWidth > 450 ? 12 : 7; // Ajustar a 10px si es mayor a 360px, sino 7px
+                                          },
                                         },
-                                        },
-                                        y: {
-                                        beginAtZero: true,
-                                        stacked: true, // Apilar las barras en el eje Y
-                                        },
+                                      },
                                     },
-                                    plugins: {
-                                        tooltip: {
-                                        enabled: true,
-                                        callbacks: {
-                                            label: function (tooltipItem) {
-                                            return tooltipItem.dataset.label + ': ' + tooltipItem.raw.toLocaleString(); // Mostrar ventas en formato de moneda
-                                            },
-                                        },
-                                        },
+                                    y: {
+                                      beginAtZero: true,
+                                      stacked: true, // Apilar las barras en el eje Y
                                     },
+                                  },
+                                  plugins: {
+                                    tooltip: {
+                                      enabled: true,
+                                      callbacks: {
+                                        label: function (tooltipItem) {
+                                          return tooltipItem.dataset.label + ': ' + tooltipItem.raw.toLocaleString(); // Mostrar ventas en formato de moneda
+                                        },
+                                      },
+                                    },
+                                    datalabels: {
+                                      anchor: "end", // Posicionar la etiqueta al final de la barra
+                                      align: "start", // Alineación a la parte superior de la barra
+                                      color: "white", // Color de las etiquetas
+                                      font: {
+                                        size: function () {
+                                          const chartWidth = window.innerWidth;
+                                          return chartWidth > 450 ? 10 : 7; // Ajustar tamaño de datalabels dinámicamente
+                                        },
+                                        weight: "bold", // Fuente en negrita
+                                      },
+                                      formatter: function (value) {
+                                        return value.toLocaleString(); // Formatear el valor de las ventas como número
+                                      },
+                                    },
+                                  },
                                 },
-                                });
+                              });
+
+
+
+
                             },
                             
                             error: function(error) {
