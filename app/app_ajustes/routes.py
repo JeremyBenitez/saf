bda9911faba9201765aed86cde6848248ced15f4
Roneyd21 <roneyalexander21@gmail.com>
2025-02-13 16:51:13 -0400

    Rutas, Modulos y Ajustes

---
+++ app/app_ajustes/routes.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template,request,jsonify
+import sqlite3
+app_ajustes = Blueprint('app_ajustes', __name__)
+
+@app_ajustes.route('/') 
+def index():
+
+    return render_template('ajustes.html')
